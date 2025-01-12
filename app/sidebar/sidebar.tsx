5317ce98c88e66c59eaae5f04376f192400e1265
Josh Smith <79778337+jshsmth@users.noreply.github.com>
2025-01-13 00:43:11 +1100

    [finance-003]: Apply sidebar styling

--- app/sidebar/sidebar.tsx
+++ app/sidebar/sidebar.tsx
@@ -4,11 +4,11 @@ import *  as React from "react";
 import { House, Cube } from "@phosphor-icons/react";
 
 const navigation = [
-  { name: 'Overview', href: '#', icon: House, current: true },
-  { name: 'Transactions', href: '#', icon: Cube, current: false },
-  { name: 'Budgets', href: '#', icon: Cube, current: false },
-  { name: 'Pots', href: '#', icon: Cube, current: false },
-  { name: 'Recurring Bills', href: '#', icon: Cube, current: false },
+  { name: 'Overview', href: '#', icon: <House size={24} weight="fill" />, current: true },
+  { name: 'Transactions', href: '#', icon: <Cube size={24} />, current: false },
+  { name: 'Budgets', href: '#', icon: <Cube size={24} />, current: false },
+  { name: 'Pots', href: '#', icon: <Cube size={24} />, current: false },
+  { name: 'Recurring Bills', href: '#', icon: <Cube size={24} />, current: false },
 ]
 
 function classNames(...classes: string[]) {
@@ -17,53 +17,60 @@ function classNames(...classes: string[]) {
 
 
 interface SidebarProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function Sidebar({ children }: SidebarProps) {
 
   return (
     <React.Fragment>
-        {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-            <div className="flex h-16 shrink-0 items-center">
-              <img
-                alt="Your Company"
-                src="https://filedn.com/llQAcN2w6V9BAgE3sxSdnCJ/finance_app/logo.svg"
-                className="h-5 w-32"
-              />
-            </div>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col bg-grey-900 rounded-r-3xl">
+        <div className="flex grow flex-col gap-y-4 overflow-y-auto pl-0 pr-8">
+          <div className="flex h-24 shrink-0 items-center pl-8 pr-4">
+            <img
+              alt="Your Company"
+              src="https://filedn.com/llQAcN2w6V9BAgE3sxSdnCJ/finance_app/logo.svg"
+              className="h-8 w-32"
+            />
+          </div>
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" className="space-y-1">
+                  {navigation.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-beige-100 text-beige-500 border-l-4 border-secondary-green text-preset-3'
+                            : 'text-grey-300 hover:bg-gray-300 hover:text-white text-preset-3',
+                          'group flex gap-x-4 rounded-r-lg px-6 py-4 text-base/6 font-medium',
+                        )}
+                      >
+                        <span
                           className={classNames(
-                            item.current
-                              ? 'bg-gray-800 text-white'
-                              : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                            'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                            'shrink-0',
+                            item.current ? 'text-secondary-green' : 'text-grey-300'
                           )}
                         >
-                          <item.icon aria-hidden="true" className="size-6 shrink-0" />
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
+                          {item.icon}
+                        </span>
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+          </nav>
         </div>
+      </div>
 
-        <main className="py-10 lg:pl-72">
-          <div className="px-4 sm:px-6 lg:px-8">{children}</div>
-        </main>
+      <main className="py-10 lg:pl-72">
+        <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+      </main>
     </React.Fragment>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,43 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      beige: {
+        100: "#F8F4F0",
+        500: "#98908B",
+      },
+      grey: {
+        100: "#F2F2F2",
+        300: "#B3B3B3",
+        500: "#696868",
+        900: '#201F24'
+      },
+      secondary: {
+        green: '#277C78',
+        yellow: '#F2CDAC',
+        cyan: '#82C9D7',
+        navy: '#626070',
+        red: '#C94736',
+        purple: '#826CB0',
+      },
+      other: {
+        purple: '#AF81BA',
+        turquoise: '#597C7C',
+        brown: '#93674F',
+        magenta: '#934F6F',
+        blue: '#3F82B2',
+        navyGrey: '#97A0AC',
+        armyGreen: '#7F9161',
+        gold: '#CAB361',
+        orange: '#BE6C49',
+      },
+      white: '#FFFFFF',
+    },
     extend: {
       fontFamily: {
         sans: [
-          '"Inter"',
+          "Public Sans",
+          "Inter",
           "ui-sans-serif",
           "system-ui",
           "sans-serif",
@@ -15,6 +48,13 @@ export default {
           '"Segoe UI Symbol"',
           '"Noto Color Emoji"',
         ],
+      },
+      fontSize: {
+        'preset-1': ['32px', { lineHeight: '120%', letterSpacing: '0px' }],
+        'preset-2': ['20px', { lineHeight: '120%', letterSpacing: '0px' }],
+        'preset-3': ['16px', { lineHeight: '150%', letterSpacing: '0px' }],
+        'preset-4': ['14px', { lineHeight: '150%', letterSpacing: '0px' }],
+        'preset-5': ['12px', { lineHeight: '150%', letterSpacing: '0px' }],
       },
     },
   },
