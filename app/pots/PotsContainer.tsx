ec3fb58bfa6fcf8f3d0d9be2a586a86a17a3700c
Josh Smith <79778337+jshsmth@users.noreply.github.com>
2025-01-26 17:35:56 +1100

    [finance-50]: Implement savings and gift card for pots section

--- app/pots/PotsContainer.tsx
+++ app/pots/PotsContainer.tsx
@@ -3,12 +3,21 @@ import { GiftSection } from "./GiftSection";
 import { HolidaySection } from "./HolidaySection";
 import { ConcertTicketSection } from "./ConcertTicketSection";
 import { NewLaptopSection } from "./NewLaptopSection";
+import { Plus } from "@phosphor-icons/react";
 
 export function PotsContainer() {
   return (
     <div>
-      <h2 className="text-preset-1 font-semibold">Pots</h2>
-      <div className="grid grid-cols-2 gap-4 mt-8">
+      <div className="flex justify-between items-center">
+        <h2 className="text-preset-1 font-semibold">Pots</h2>
+        <button className="text-preset-4 font-semibold bg-grey-900 hover:bg-grey-500 text-white p-4 rounded-md">
+          <div className="flex items-center gap-1">
+            <Plus size={12} />
+            <span>Add New Pot</span>
+          </div>
+        </button>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 mt-8">
         <div className="space-y-6">
           <SavingsSection />
           <GiftSection />
