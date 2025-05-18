5f2519eb65d902bad437a76836aa8971bdcc0a72
jshsmth <79778337+jshsmth@users.noreply.github.com>
2025-05-18 15:10:02 +1000

    chore: remove netify config file in favour of vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     ssr: true
   }
 });
